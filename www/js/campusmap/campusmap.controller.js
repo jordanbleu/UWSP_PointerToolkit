962ea77a1bbb97ec3fa19e17628627dfbf3a7b09
Jordan Bleu <Jordan Bleu>
2017-05-07 15:05:03 -0500

    fixed stupid ionic stupidness

--- www/js/campusmap/campusmap.controller.js
+++ www/js/campusmap/campusmap.controller.js
@@ -475,7 +475,6 @@
         function setWidth() {
             var imageWidth = document.querySelector("#thumbnail").width;
             document.getElementById("addressText").style.width = imageWidth + "px";
-            document.getElementById("addressText").style.marginLeft = (-imageWidth / 2) + "px";
         }
 
         // Modal destruction
@@ -488,40 +487,42 @@
 
         // Creates the Location control with the approriate settings and adds it to the map
         function loadLocationControl(leafletMap) {
-            var locateControl = L.control.locate({
-                position: 'topleft',  // set the location of the control
-                layer: svg,  // use your own layer for the location marker
-                drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
-                follow: true,  // follow the user's location
-                setView: true, // automatically sets the map view to the user's location, enabled if `follow` is true
-                keepCurrentZoomLevel: true, // keep the current map zoom level when displaying the user's location. (if `false`, use maxZoom)
-                stopFollowingOnDrag: true, // stop following when the map is dragged if `follow` is true (deprecated, see below)
-                remainActive: false, // if true locate control remains active on click even if the user's location is in view.
-                markerClass: L.circleMarker, // L.circleMarker or L.marker
-                circleStyle: {},  // change the style of the circle around the user's location
-                markerStyle: {},
-                followCircleStyle: {},  // set difference for the style of the circle around the user's location while following
-                followMarkerStyle: {},
-                icon: 'fa fa-map-marker',  // class for icon, fa-location-arrow or fa-map-marker
-                iconLoading: 'fa fa-spinner fa-spin',  // class for loading icon
-                circlePadding: [0, 0], // padding around accuracy circle, value is passed to setBounds
-                metric: false,  // use metric or imperial units
-                onLocationError: function (err) { alert(err.message) },  // define an error callback function
-                onLocationOutsideMapBounds: function (context) { // called when outside map boundaries
-                    alert(context.options.strings.outsideMapBoundsMsg);
-                },
-                showPopup: true, // display a popup when the user click on the inner marker
-                strings: {
-                    title: "Show me where I am",  // title of the locate control
-                    metersUnit: "meters", // string for metric units
-                    feetUnit: "feet", // string for imperial units
-                    popup: "You are within {distance} {unit} from this point",  // text to appear if user clicks on circle
-                    outsideMapBoundsMsg: "You seem located outside the boundaries of the map" // default message for onLocationOutsideMapBounds
-                },
-                locateOptions: { enableHighAccuracy: true }  // define location options e.g enableHighAccuracy: true or maxZoom: 10
-            });
+            if ($(".leaflet-bar-part-single").length == 0) {
+                var locateControl = L.control.locate({
+                    position: 'topleft',  // set the location of the control
+                    layer: svg,  // use your own layer for the location marker
+                    drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
+                    follow: true,  // follow the user's location
+                    setView: true, // automatically sets the map view to the user's location, enabled if `follow` is true
+                    keepCurrentZoomLevel: true, // keep the current map zoom level when displaying the user's location. (if `false`, use maxZoom)
+                    stopFollowingOnDrag: true, // stop following when the map is dragged if `follow` is true (deprecated, see below)
+                    remainActive: false, // if true locate control remains active on click even if the user's location is in view.
+                    markerClass: L.circleMarker, // L.circleMarker or L.marker
+                    circleStyle: {},  // change the style of the circle around the user's location
+                    markerStyle: {},
+                    followCircleStyle: {},  // set difference for the style of the circle around the user's location while following
+                    followMarkerStyle: {},
+                    icon: 'fa fa-map-marker',  // class for icon, fa-location-arrow or fa-map-marker
+                    iconLoading: 'fa fa-spinner fa-spin',  // class for loading icon
+                    circlePadding: [0, 0], // padding around accuracy circle, value is passed to setBounds
+                    metric: false,  // use metric or imperial units
+                    onLocationError: function (err) { alert(err.message) },  // define an error callback function
+                    onLocationOutsideMapBounds: function (context) { // called when outside map boundaries
+                        alert(context.options.strings.outsideMapBoundsMsg);
+                    },
+                    showPopup: true, // display a popup when the user click on the inner marker
+                    strings: {
+                        title: "Show me where I am",  // title of the locate control
+                        metersUnit: "meters", // string for metric units
+                        feetUnit: "feet", // string for imperial units
+                        popup: "You are within {distance} {unit} from this point",  // text to appear if user clicks on circle
+                        outsideMapBoundsMsg: "You seem located outside the boundaries of the map" // default message for onLocationOutsideMapBounds
+                    },
+                    locateOptions: { enableHighAccuracy: true }  // define location options e.g enableHighAccuracy: true or maxZoom: 10
+                });
 
-            locateControl.addTo(leafletMap);
+                locateControl.addTo(leafletMap);
+            }
         };//end loadLocationControl
 
 
